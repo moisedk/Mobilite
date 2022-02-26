c0ce71b81206e5ad67e6bdfd6f1307708ed78266
Moise Dete-Kpinssounon <moisedete@gmail.com>
2022-02-26 01:25:14 -0500

    Added three fragments for screen: Welcome, Register & Login Screens

--- app/src/main/java/com/moise/mobilite/ui/LoginActivity.kt
+++ app/src/main/java/com/moise/mobilite/ui/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.moise.mobilite
+package com.moise.mobilite.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.moise.mobilite.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
