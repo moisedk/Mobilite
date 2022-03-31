27ac2d03702ec87577479d924540fc35ea72409b
Moise Dete-Kpinssounon <moisedete@gmail.com>
2022-03-31 02:42:41 -0400

    Sh*t is about to get real

--- app/src/main/java/com/moise/mobilite/ui/HomeMapsActivity.kt
+++ app/src/main/java/com/moise/mobilite/ui/HomeMapsActivity.kt
@@ -1,16 +1,22 @@
 package com.moise.mobilite.ui
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.navigation.NavigationView
 import com.moise.mobilite.R
 import com.moise.mobilite.databinding.ActivityHomeMapsBinding
 
@@ -19,22 +25,43 @@ class HomeMapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityHomeMapsBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         drawerLayout = binding.drawer
-        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val navView: NavigationView = binding.navigationView
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.map,
+                R.id.nav_payments,
+                R.id.nav_history,
+                R.id.nav_free_journeys,
+                R.id.nav_loyalty_points,
+                R.id.nav_promotions,
+                R.id.nav_work_rides,
+                R.id.nav_support,
+                R.id.nav_about
+            ), drawerLayout
+        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         mapFragment.setHasOptionsMenu(true)
+
     }
 
     /**
@@ -55,10 +82,15 @@ class HomeMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
+            return true
         }
         return super.onOptionsItemSelected(item)
     }
---
+++ app/src/main/java/com/moise/mobilite/ui/PlugToFragment.kt
@@ -0,0 +1,8 @@
+package com.moise.mobilite.ui
+
+import androidx.fragment.app.Fragment
+
+
+class PlugToFragment : Fragment() {
+
+}
