27f7d0e2b51ae49b5f8c0470af727d23ecd8a2d2
Moise Dete-Kpinssounon <moisedete@gmail.com>
2022-04-01 19:58:31 -0400

    Magnificat

--- app/src/main/java/com/moise/mobilite/ui/HomeMapsActivity.kt
+++ app/src/main/java/com/moise/mobilite/ui/HomeMapsActivity.kt
@@ -3,6 +3,7 @@ package com.moise.mobilite.ui
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -20,12 +21,10 @@ import com.google.android.material.navigation.NavigationView
 import com.moise.mobilite.R
 import com.moise.mobilite.databinding.ActivityHomeMapsBinding
 
-class HomeMapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class HomeMapsActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
-    private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityHomeMapsBinding
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,61 +36,15 @@ class HomeMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        val navView: NavigationView = binding.navigationView
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.map,
-                R.id.nav_payments,
-                R.id.nav_history,
-                R.id.nav_free_journeys,
-                R.id.nav_loyalty_points,
-                R.id.nav_promotions,
-                R.id.nav_work_rides,
-                R.id.nav_support,
-                R.id.nav_about
-            ), drawerLayout
-        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
+        val navigationView: NavigationView = binding.navigationView
 
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-        mapFragment.setHasOptionsMenu(true)
-
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true
+        navigationView.setNavigationItemSelectedListener {
+            item ->
+            if (item.itemId == R.id.nav_plug_to)
+                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+            true
         }
-        return super.onOptionsItemSelected(item)
+
     }
+
 }
