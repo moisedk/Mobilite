6725ca586b49f72bf0945e7857637693c03d8cd0
Moise Dete-Kpinssounon <moisedete@gmail.com>
2022-04-16 01:45:16 -0400

    Created a DataModel for a ride

--- app/src/main/java/com/moise/mobilite/ui/Ride.kt
+++ app/src/main/java/com/moise/mobilite/ui/Ride.kt
@@ -2,5 +2,4 @@ package com.moise.mobilite.ui
 
 import java.util.*
 
-data class Ride(val description: String, val date: Date, val completed: Boolean) {
-}
+data class Ride(val description: String, val date: Date, val completed: Boolean)
