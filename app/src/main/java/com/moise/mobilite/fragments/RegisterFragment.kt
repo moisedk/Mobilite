add705203ece93bc16a6bc143bf02591a199c7c0
Moise Dete-Kpinssounon <moisedete@gmail.com>
2022-05-06 19:08:02 -0400

    Reimplement color design

--- app/src/main/java/com/moise/mobilite/fragments/RegisterFragment.kt
+++ app/src/main/java/com/moise/mobilite/fragments/RegisterFragment.kt
@@ -1,6 +1,12 @@
 package com.moise.mobilite.fragments
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +16,7 @@ import androidx.navigation.fragment.findNavController
 import com.moise.mobilite.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+    private val terms: CharSequence = "By signing up, you agree to Photo's Terms of Service and Privacy Policy"
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
 
@@ -34,5 +41,25 @@ class RegisterFragment : Fragment() {
                 Toast.LENGTH_SHORT
             ).show()
         }
+        val spannableString = SpannableString(terms)
+        val clickableSpan = object : ClickableSpan() {
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+            }
+
+            override fun onClick(widget: View) {
+                Toast.makeText(requireContext(), "Terms and services", Toast.LENGTH_SHORT).show()
+            }
+        }
+        spannableString.setSpan(clickableSpan, terms.indexOf("Terms"), terms.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.tvTerms.text = spannableString
+        binding.tvTerms.movementMethod = LinkMovementMethod.getInstance()
+        binding.tvTerms.highlightColor = Color.TRANSPARENT
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
